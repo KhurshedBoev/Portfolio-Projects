81a364806a7ebb365d1e7f0fdbf5c09a059199b4
KhurshedBoev <72939670+KhurshedBoev@users.noreply.github.com>
2023-05-15 07:24:29 -0400

    Add 1st array

---
+++ app.js
@@ -0,0 +1,23 @@
+/* We will first have array of the inspirational words,
+where we will randomly select and put them together.
+Array #1 - "Stay hungry. Stay foolish." - Steve Jobs, 
+"Believe and act as if it were impossible to fail." - Charles Kettering, 
+"Be yourself." - Oscar Wilde, "Dream big. Start small." - Simon Sinek, 
+"You must do the thing you think you cannot do." - Eleanor Roosevelt
+
+Array #2 - "The best way to predict the future is to create it." - Abraham Lincoln, 
+"Believe you can and you're halfway there." - Theodore Roosevelt, 
+"In the middle of every difficulty lies opportunity." - Albert Einstein, 
+"It does not matter how slowly you go as long as you do not stop." - Confucius, 
+"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt
+
+Array #3 - Go for it!, Just do it!, Dream BIG!, "Believe and achieve!", "Carpe diem!", 
+"Push the limits!", "Never give up!", "Keep going!", "Stay focused!", "Own your life." - Oprah Winfrey
+
+*/
+
+
+
+let inspire1 = ['"Stay hungry. Stay foolish." - Steve Jobs', '"Believe and act as if it were impossible to fail." - Charles Kettering', '"Be yourself." - Oscar Wilde', '"Dream big. Start small." - Simon Sinek', '"You must do the thing you think you cannot do." - Eleanor Roosevelt']
+
+console.log(inspire1)
