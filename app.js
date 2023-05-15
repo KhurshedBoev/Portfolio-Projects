34edaddb7e40187267095dfc3177fe211109e0ac
KhurshedBoev <72939670+KhurshedBoev@users.noreply.github.com>
2023-05-15 07:55:34 -0400

    Add all arrays

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ Array #3 - Go for it!, Just do it!, Dream BIG!, "Believe and achieve!", "Carpe d
 
 
 
-let inspire1 = ['"Stay hungry. Stay foolish." - Steve Jobs', '"Believe and act as if it were impossible to fail." - Charles Kettering', '"Be yourself." - Oscar Wilde', '"Dream big. Start small." - Simon Sinek', '"You must do the thing you think you cannot do." - Eleanor Roosevelt']
+let inspire1 = ['"Stay hungry. Stay foolish." - Steve Jobs', '"Believe and act as if it were impossible to fail." - Charles Kettering', '"Be yourself." - Oscar Wilde', '"Dream big. Start small." - Simon Sinek', '"You must do the thing you think you cannot do." - Eleanor Roosevelt'];
 
-console.log(inspire1)
+
+let inspire2 = ['"The best way to predict the future is to create it." - Abraham Lincoln', '"Believe you can and you are halfway there." - Theodore Roosevelt', '"In the middle of every difficulty lies opportunity." - Albert Einstein', '"It does not matter how slowly you go as long as you do not stop." - Confucius', '"The future belongs to those who believe in the beauty of their dreams." - Eleanor Roosevelt'];
+
+let inspire3 = ['Go for it!', 'Just do it!', 'Dream BIG!', '"Believe and achieve!"', '"Carpe diem!"', 
+'"Push the limits!"', '"Never give up!"', '"Keep going!"', '"Stay focused!"', '"Own your life." - Oprah Winfrey'];
+
+console.log(inspire3)
